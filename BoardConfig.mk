f4865874e0de49bdbebe4a13a0e55b9a9f03bd94
yashsonawane <yash050219991@gmail.com>
2020-01-23 16:04:49 +0000

    star2qltechn: Use pixel fp to bypass safetynet
    Signed-off-by: yashsonawane <yash050219991@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,4 @@
 
-
 # Inherit from samsung sdm845-common
 -include device/samsung/sdm845-common/BoardConfigCommon.mk
 
@@ -10,3 +9,7 @@ TARGET_OTA_ASSERT_DEVICE := star2qltechn
 
 # Kernel
 TARGET_KERNEL_CONFIG := star2qlte_chn_open_defconfig
+
+# Vendor init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_star2qltechn
+TARGET_RECOVERY_DEVICE_MODULES := libinit_star2qltechn
