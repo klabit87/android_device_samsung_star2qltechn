865d897a48daccacf3952b7a044421ef6b1af150
klabit87 <keith.labit@gmail.com>
2020-05-11 22:32:20 -0500

    Update fingerprint

--- lineage_star2qltechn.mk
+++ lineage_star2qltechn.mk
@@ -9,6 +9,10 @@ BOARD_VENDOR := samsung
 # Build fingerprint
 ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	BUILD_FINGERPRINT=samsung/star2qltezh/star2qltechn:10/QP1A.190711.020/G9650ZHU6DTA7:user/release-keys \
-	PRIVATE_BUILD_DESC="star2qltezh-user 10 QP1A.190711.020 G9650ZHU6DTA7 release-keys"
+	BUILD_FINGERPRINT=samsung/star2qltezh/star2qltechn:10/QP1A.190711.020/G9650ZHU6DTD1:user/release-keys \
+	PRIVATE_BUILD_DESC="star2qltezh-user 10 QP1A.190711.020 G9650ZHU6DTD1 release-keys"
 endif
+
+# OTA
+PRODUCT_PROPERTY_OVERRIDES += \
+	lineage.updater.uri=https://raw.githubusercontent.com/klabit87/OTA/master/star2qltechn.json
