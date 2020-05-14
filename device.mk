de3d3f46b7114dfb495152b83534bde9a2ab3200
klabit87 <keith.labit@gmail.com>
2020-05-13 20:24:15 -0500

    Add power profile

--- device.mk
+++ device.mk
@@ -1,6 +1,7 @@
 
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
+
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Inherit common device configuration
 $(call inherit-product, device/samsung/sdm845-common/common.mk)
