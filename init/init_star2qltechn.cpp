4093c718ce4f78ebab8b70d781a7594d739ac97f
klabit87 <keith.labit@gmail.com>
2020-11-16 17:56:35 -0600

    Safetynet hardware attestation patch

--- init/init_star2qltechn.cpp
+++ init/init_star2qltechn.cpp
@@ -61,4 +61,9 @@ void vendor_load_properties()
 
     // privapp permisison control
     property_override("ro.control_privapp_permissions", "log");
+    
+    property_override("ro.product.system.model", "BASIC");
+    property_override("ro.product.vendor.model", "BASIC");
+    property_override("ro.product.product.model", "BASIC");
+    property_override("ro.product.odm.model", "BASIC");
 }
